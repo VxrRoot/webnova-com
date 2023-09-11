d0ee09981830597ad5e48b4384e65ec232814359
Dawid Słowik <dawid.slowik@visionect.io>
2023-09-11 16:48:07 +0200

    update

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -6,23 +6,23 @@ import Link from "next/link";
 import SocialIcons from "../social-icons/SocialIcons";
 
 const Header: FC = () => {
-    return (
-        <header className="w-full z-20 fixed flex flex-col top-0 left-0 backdrop-blur-lg padding-x py-4 h-16 lg:py-5 bg-black/50 lg:m-auto lg:h-20 lg:flex-row">
-            <div className="m-auto flex justify-between w-full lg:max-w-7xl">
-                <div
-                    className={`w-auto max-h-8 lg:w-56  ${styles.logo_wr} hover:cursor-pointer `}
-                >
-                    <Link href="/">
-                        <Logo />
-                    </Link>
-                </div>
-                <Nav />
-                <div className="flex items-center">
-                    <SocialIcons styles="w-[220px]" />
-                </div>
-            </div>
-        </header>
-    );
+  return (
+    <header className="w-full z-20 fixed flex flex-col top-0 left-0 backdrop-blur-lg padding-x py-4 h-16 lg:py-5 bg-black/50 lg:m-auto lg:h-20 lg:flex-row">
+      <div className="m-auto flex justify-between w-full lg:max-w-7xl">
+        <div
+          className={`w-auto max-h-8 lg:w-56 ${styles.logo_wr} hover:cursor-pointer `}
+        >
+          <Link href="/">
+            <Logo />
+          </Link>
+        </div>
+        <Nav />
+        <div className="flex items-center">
+          <SocialIcons styles="w-[220px]" />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
