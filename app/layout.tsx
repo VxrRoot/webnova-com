42bb465fe62fe21a4f0c2e81b4020a9c5242f9fb
Dawid Słowik <dawid.slowik@visionect.io>
2023-10-15 00:24:11 +0200

    Add favicon, update opinions and update meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,28 +3,61 @@ import { Inter } from "@next/font/google";
 
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
 
+export const metadata: Metadata = {
+  icons: {
+    icon: "/favicons/favicon.ico",
+  },
+  keywords: [
+    "Tworzenie stron internetowych",
+    "strony internetowe",
+    "webflow",
+    "nextjs",
+    "react",
+    "Strony internetowe na zamówienie",
+    "Projektowanie stron internetowych",
+    "Strony internetowe Webflow",
+    "Webflow development",
+    "Strony internetowe dla biznesu",
+    "Strony internetowe dla małych firm",
+    "Responsywne strony internetowe",
+    "SEO i optymalizacja stron",
+    "Landing page na Webflow",
+    "Szybkie strony internetowe",
+    "Portfolio stron internetowych",
+    "Strony internetowe dla restauracji",
+    "Strony internetowe dla agencji reklamowych",
+  ],
+  openGraph: {
+    locale: "pl_PL",
+    url: "https://www.web-nova.pl/",
+    title: "Skuteczne strony internetowe",
+    description:
+      "Tworzymy dedykowane strony internetowe, które pomagają naszym klientom maksymalizować zyski oraz realizować cele w ich biznesach",
+  },
+};
+
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="pl">
-            <head />
-            <body
-                // className={`bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black ${inter.className} text-white`}
-                className={`bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black ${inter.className}`}
-            >
-                <div id="stars"></div>
-                <div id="stars2"></div>
-                <div id="stars3"></div>
-                <Header />
-                {children}
-                <Footer />
-            </body>
-        </html>
-    );
+  return (
+    <html lang="pl">
+      <head />
+      <body
+        className={`bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black ${inter.className}`}
+      >
+        <div id="stars"></div>
+        <div id="stars2"></div>
+        <div id="stars3"></div>
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
 }
--- src/sections/realizations-section/RealizationsSection.tsx
+++ src/sections/realizations-section/RealizationsSection.tsx
@@ -6,56 +6,48 @@ import SectionHeadline from "@/app/components/headlines/SectionHeadline";
 import SingleRealization from "./elements/SingleRealization";
 
 const realizations = [
-    {
-        title: "mikolajulikowski.pl",
-        description:
-            "Strona stworzona dla Mikołaja. Celem strony jest zbieranie kontaktów do klientów. Strona została stworzona w oparciu o Next.js. Strona pomaga Mikołajowi zdobywać więcej klientów.",
-        img: MikolajulikowskiImg,
-        altText: "Mikolajulikowski.pl",
-        link: "https://www.mikolajulikowski.pl/",
-    },
-    {
-        title: "tattooflow.pl",
-        description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-        img: TattooflowImg,
-        altText: "medium.com",
-        link: "https://tattooflow.pl/",
-    },
-    {
-        title: "facebook.com",
-        description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-        img: MikolajulikowskiImg,
-        altText: "Mikolajulikowski.pl",
-        link: "/",
-    },
+  {
+    title: "mikolajulikowski.pl",
+    description:
+      "Strona stworzona dla Mikołaja. Celem strony jest zbieranie kontaktów do klientów. Strona została stworzona w oparciu o Next.js. Strona pomaga Mikołajowi zdobywać więcej klientów.",
+    img: MikolajulikowskiImg,
+    altText: "Mikolajulikowski.pl",
+    link: "https://www.mikolajulikowski.pl/",
+  },
+  {
+    title: "tattooflow.pl",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+    img: TattooflowImg,
+    altText: "medium.com",
+    link: "https://tattooflow.pl/",
+  },
 ];
 
 interface IRealizationsSection {}
 
 const RealizationsSection: FC<IRealizationsSection> = () => {
-    return (
-        <div className="padding-x bg-backgroundDark">
-            <section className="max-w-7xl w-full m-auto py-20 ">
-                <SectionHeadline text="Sprawdź moje realizacje" />
-                <div className="grid grid-cols-1 pt-9 gap-6 md:grid-cols-2 lg:grid-cols-3">
-                    {realizations.map(
-                        ({ altText, description, img, title, link }, idx) => (
-                            <SingleRealization
-                                altText={altText}
-                                description={description}
-                                img={img}
-                                title={title}
-                                key={idx}
-                                link={link}
-                            />
-                        )
-                    )}
-                </div>
-            </section>
+  return (
+    <div className="padding-x bg-backgroundDark">
+      <section className="max-w-7xl w-full m-auto py-20 ">
+        <SectionHeadline text="Sprawdź moje realizacje" />
+        <div className="grid grid-cols-1 pt-9 gap-6 md:grid-cols-2 lg:grid-cols-3">
+          {realizations.map(
+            ({ altText, description, img, title, link }, idx) => (
+              <SingleRealization
+                altText={altText}
+                description={description}
+                img={img}
+                title={title}
+                key={idx}
+                link={link}
+              />
+            )
+          )}
         </div>
-    );
+      </section>
+    </div>
+  );
 };
 
 export default RealizationsSection;
