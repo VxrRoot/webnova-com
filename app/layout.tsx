4484425b5f2e8ff9e8718985ad6f7f5a67c65d72
Dawid Slowik <dawidslowik379@gmail.com>
2024-08-28 20:52:11 +0200

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,6 @@ import dynamic from "next/dynamic";
 import CookieBanner from "@/src/components/cookies/CookieBanner";
 import GoogleAnalytics from "@/src/components/GA/GoogleAnalitics";
 import { Suspense } from "react";
-import Script from "next/script";
 // const CookieBanner = dynamic(
 //     () => import("../src/components/cookies/CookieBanner"),
 //     { ssr: false }
@@ -77,25 +76,6 @@ export default function RootLayout({
       <Suspense>
         <GoogleAnalytics GA_MEASUREMENT_ID={process.env.GA_MEASUREMENT_ID!} />
       </Suspense>
-      <head>
-        <Script
-          src="https://app.easycart.pl/login.js?type=block&id=prod_QkLJWPiiztHE4P"
-          strategy="beforeInteractive" // Ładowanie skryptu przed interaktywnością strony
-        />
-
-        {/* Skrypt inline w sekcji head */}
-        <Script id="easycart-redirect-script" strategy="beforeInteractive">
-          {`!window._EC_HASH_8c817de8a8fe287f0a650048efd4b605 && (location.href = "https://app.easycart.pl/r/prod_QkLJWPiiztHE4P");`}
-        </Script>
-
-        {/* Noscript element w sekcji head */}
-        <noscript>
-          <meta
-            httpEquiv="refresh"
-            content="0;url=https://app.easycart.pl/r/prod_QkLJWPiiztHE4P"
-          />
-        </noscript>
-      </head>
       <body className={`relative ${sans.className}`}>
         {/* <div id="stars"></div>
                 <div id="stars2"></div>
